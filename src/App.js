e36e3481dcb120a3ad9bee421c675ec730779473
Marcin <marcin.p.dev@gmail.com>
2022-10-27 13:29:32 +0200

    Cleaned up code, removed logs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import "./App.css";
-import Timer from './Timer.js';
+import Timer from "./Timer.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Timer />
-
+        <Timer />
       </header>
     </div>
   );
